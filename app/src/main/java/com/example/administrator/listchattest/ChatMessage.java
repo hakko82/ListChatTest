cace29fcd1fc8527b70f4874ea380b9a7622d4d9
hakko82@gmail.com <hakko7992>
2015-12-16 16:17:25 +0900

    chat source

--- app/src/main/java/com/example/administrator/listchattest/ChatMessage.java
+++ app/src/main/java/com/example/administrator/listchattest/ChatMessage.java
@@ -4,53 +4,12 @@ package com.example.administrator.listchattest;
  * Created by Administrator on 2015-12-14.
  */
 public class ChatMessage {
-    private long id;
-    private boolean isMe;
-    private String message;
-    private Long userId;
-    private String dateTime;
+    public boolean left;
+    public String message;
 
-    public ChatMessage(String message) {
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
         this.message = message;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public boolean getIsme() {
-        return isMe;
-    }
-
-    public void setMe(boolean isMe) {
-        this.isMe = isMe;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    public String getDate() {
-        return dateTime;
-    }
-
-    public void setDate(String dateTime) {
-        this.dateTime = dateTime;
     }
 }
--- app/src/main/java/com/example/administrator/listchattest/MainActivity.java
+++ app/src/main/java/com/example/administrator/listchattest/MainActivity.java
@@ -1,90 +1,72 @@
 package com.example.administrator.listchattest;
 
 import android.app.Activity;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 
-public class MainActivity extends Activity{
-    ChatMessageAdapter chatMessageAdapter;
+public class MainActivity extends Activity {
+    private static final String TAG = "ChatActivity";
+
+    private ChatArrayAdapter chatArrayAdapter;
+    private ListView listView;
+    private EditText chatText;
+    private Button buttonSend;
+    private boolean side = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        chatMessageAdapter = new ChatMessageAdapter(this.getApplicationContext(),R.layout.chatting_message);
+        buttonSend = (Button) findViewById(R.id.send);
+        listView = (ListView) findViewById(R.id.msgview);
+        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.right);
+        listView.setAdapter(chatArrayAdapter);
 
-        Button btn = (Button) findViewById(R.id.buttonSend);
-        btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                send(v);
+        chatText = (EditText) findViewById(R.id.msg);
+        chatText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    return sendChatMessage();
+                }
+                return false;
             }
         });
-    }
+        
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                sendChatMessage();
+            }
+        });
 
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
 
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        chatMessageAdapter = new ChatMessageAdapter(this.getApplicationContext(),R.layout.chatting_message);
-        final ListView listView = (ListView)findViewById(R.id.listView1);
-        listView.setAdapter(chatMessageAdapter);
-        listView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL); // 이게 필수
-
-        // When message is added, it makes listview to scroll last message
-        chatMessageAdapter.registerDataSetObserver(new DataSetObserver() {
+        //to scroll the list view to bottom on data change
+        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
             @Override
             public void onChanged() {
                 super.onChanged();
-                listView.setSelection(chatMessageAdapter.getCount()-1);
+                listView.setSelection(chatArrayAdapter.getCount() - 1);
             }
         });
-        return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private boolean sendChatMessage() {
+        if ( !chatText.getText().toString().trim().equals("") ) {
+            chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));
+            chatText.setText("");
+            side = !side;
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-*/
-
-
-    public void send(View view){
-        EditText etMsg = (EditText)findViewById(R.id.chatText);
-        String strMsg = (String)etMsg.getText().toString();
-        chatMessageAdapter.add(new ChatMessage(strMsg));
-
-        final ListView listView = (ListView)findViewById(R.id.listView1);
-        listView.setAdapter(chatMessageAdapter);
-        listView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL); // 이게 필수
-        listView.setSelection(chatMessageAdapter.getCount() - 1);
-
-        /*
-        // When message is added, it makes listview to scroll last message
-        chatMessageAdapter.registerDataSetObserver(new DataSetObserver() {
-            @Override
-            public void onChanged() {
-                super.onChanged();
-                listView.setSelection(chatMessageAdapter.getCount() - 1);
-            }
-        });
-        */
+        return true;
     }
 }
